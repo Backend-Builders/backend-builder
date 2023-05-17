d7fd717dc787db6cc668f8b50c8955bb819a0811
Paulo Henrique Lima <limapaulobsb@gmail.com>
2023-05-17 01:36:10 -0300

    feat(fs): add functionality to copy boilerplate directories

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+#!/usr/bin/env node
 import input from '@inquirer/input';
 import select from '@inquirer/select';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as fs from 'fs';
 
 import choices from './choices.js';
 
+// CLI interactions
 const projectName = await input({ message: 'Enter the project name:' });
 
 const projectType = await select({
@@ -10,4 +15,13 @@ const projectType = await select({
   choices,
 });
 
-console.log(projectName, projectType);
+// Sets project directory paths
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const boilerplatePath = `${__dirname}/boilerplates/${projectType}`;
+const projectPath = `./${projectName}`;
+
+// Copies the boilerplate structure, including subdirectories and files
+fs.cpSync(boilerplatePath, projectPath, { recursive: true });
+
+console.log(`Project ${projectName} successfully created!`);
