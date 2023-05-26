54f7b212ad70edb64ae5ee85d1f635bb4f5cf17a
Paulo Henrique Lima <limapaulobsb@gmail.com>
2023-05-26 13:01:47 -0300

    feat(cli): create license options and add files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import * as fs from 'fs';
 
-import choices from './choices.js';
+import boilerplates from './boilerplates/index.js';
+import licenses from './licenses/index.js';
 
 // CLI interactions
 const projectName = await input({
@@ -28,19 +29,18 @@ if (moreDetails) {
       description: await input({ message: 'description:' }),
       keywords: await input({ message: 'keywords:' }),
       author: await input({ message: 'author:' }),
+      license: await select({ message: 'Choose a license:', choices: licenses }),
     };
 
     projectDetails.keywords = projectDetails.keywords.split(', ');
-
     console.log(`\n${JSON.stringify(projectDetails, null, 2)}\n`);
-    
     confirmation = await confirm({ message: 'Is this information correct?' });
   } while (!confirmation);
 }
 
 const projectType = await select({
   message: 'Select the project type:',
-  choices,
+  choices: boilerplates,
 });
 
 // Set project directory paths
@@ -52,13 +52,15 @@ const projectPath = `./${projectName}`;
 // Copy boilerplate structure, including subdirectories and files
 fs.cpSync(boilerplatePath, projectPath, { recursive: true });
 
-// Change project details in the package.json file
+// Change project details in the package.json and copy license file
 const data = fs.readFileSync(`${projectPath}/package.json`, { encoding: 'utf8' });
 let dataObj = JSON.parse(data);
 dataObj.name = projectName;
 
 if (moreDetails) {
   dataObj = { ...dataObj, ...projectDetails };
+  const licensePath = `${__dirname}/licenses/${projectDetails.license}`;
+  fs.cpSync(licensePath, projectPath, { recursive: true });
 }
 
 fs.writeFileSync(`${projectPath}/package.json`, `${JSON.stringify(dataObj, null, 2)}\n`);
